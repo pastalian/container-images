b44be1a8f0afcba3d9b3bbed15f1696163fab12b
pastalian <pastalian46@gmail.com>
2024-02-03 03:57:04 +0900

    alma: add separate config.sh

--- alma/config.sh
+++ alma/config.sh
@@ -1,1 +1,14 @@
-../config.sh
+#!/bin/bash
+
+test -f /.kconfig && . /.kconfig
+test -f /.profile && . /.profile
+
+echo 'tsflags=nodocs' >> /etc/dnf/dnf.conf
+echo 'LANG="C.UTF-8"' > /etc/locale.conf
+echo '%_install_langs C.utf8' > /etc/rpm/macros.image-language-conf
+
+# Import AlmaLinux GPP key.
+rpm --import "/etc/pki/rpm-gpg/RPM-GPG-KEY-AlmaLinux-$(rpm -E '%{?almalinux}')"
+
+# Follow the convention and create an empty file.
+: > /etc/machine-id
