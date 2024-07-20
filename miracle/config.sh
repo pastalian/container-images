1ff29a71da2865ca6087726ec4b2ce7753931b96
pastalian <pastalian46@gmail.com>
2024-07-20 14:30:22 +0900

    miracle: add additional image tags

--- miracle/config.sh
+++ miracle/config.sh
@@ -3,10 +3,10 @@
 test -f /.kconfig && . /.kconfig
 test -f /.profile && . /.profile
 
-echo 'LANG="C.UTF-8"' > /etc/locale.conf
+echo 'LANG="C.utf8"' > /etc/locale.conf
 echo '%_install_langs C.utf8' > /etc/rpm/macros.image-language-conf
 
-# Import MIRACLE LINUX GPP key.
+# Import RPM GPG key.
 rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY
 
 # Follow the convention and create an empty file.
